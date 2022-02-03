5bc161b6278b3ba0f99785fe6a4938ccb84150f9
Deepak <deepak@Deepaks-MacBook-Pro.local>
2022-02-03 06:16:36 -0500

    Created a navigation bar component using vuetify and Vue. This includes a pop in slider which pops in when an icon is clicked and a sign out button.

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <v-app>
-    
+    <NavigationBar/>
+    <v-content class="ma-4">
+      <router-view></router-view>
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import NavigationBar from './components/NavigationBar.vue'
 
 export default {
-  name: 'App',
-
   components: {
-  },
-
-  data: () => ({
-    
-  }),
-};
+    NavigationBar,
+  }
+}
 </script>
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
