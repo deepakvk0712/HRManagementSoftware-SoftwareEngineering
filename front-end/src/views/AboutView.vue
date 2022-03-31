1530d8e6ee90142d42da84e9965f0430936b51ff
Deepak <deepak@Deepaks-MacBook-Pro.local>
2022-03-31 14:34:20 -0400

    Made changes to the About Us page. Added section on company accomplishments. Also made some changes to company policy page and the employee page.

--- front-end/src/views/AboutView.vue
+++ front-end/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div style="heigh: 100%;" class="about">
-    <v-container fluid style="height: 100%">
+  <div style="top:0px; height: 100%;" class="about">
+    <v-container fluid class="p-0" style="height: 100%">
       <!-- 100vh -->
       <NavigationBar class="pb-0" v-bind:userName="userName"/>
       <v-layout fill-height class="d-flex">
@@ -29,7 +29,7 @@
                 >
                   <v-tab
                     v-for="t in tabName"
-                    :key="t"
+                    :key="t.Id"
                   >
                     {{t}}
                   </v-tab>
@@ -75,37 +75,149 @@
 
                   <v-card-text>
                     <v-row>
-                      <v-col class="d-flex justify-center">
+                      <!-- align-content="center" -->
+                      <!-- align="center" -->
+                      <v-col style="text-align:center; display:inline-block;" align-content="center" cols="4" class="fill-height">
                         <!-- <span> -->
-                          <v-icon color="#D22B2B" style="font-size: 44px;" large>account_group_outline</v-icon>
+                          <v-icon color="#D22B2B">account_group_outline</v-icon>
+                          <v-card-text class="text-h4 font-weight-bold">
+                            2000+ Employees
+                          </v-card-text>
                         <!-- </span> -->
+                        <!-- large -->
                       </v-col>
 
-                      <v-col class="d-flex justify-center">
+                      <v-col style="text-align:center; display:inline-block;" align-content="center" cols="4" class="fill-height">
                         <!-- <span> -->
-                          <v-icon color="#D22B2B" style="font-size: 44px;" large>account_group_outline</v-icon>
+                          <v-icon color="#D22B2B">feed</v-icon>
+                          <v-card-text class="text-h4 font-weight-bold">
+                            50000+ Records Processed
+                          </v-card-text>
                         <!-- </span> -->
+                        <!-- style="font-size: 44px;" -->
                       </v-col>
 
-                      <v-col  class="d-flex justify-center">
+                      <v-col style="text-align:center; display:inline-block;" align-content="center" cols="4" class="fill-height">
                         <!-- <span> -->
-                          <v-icon color="#D22B2B" style="font-size: 44px;" large>account_group_outline</v-icon>
+                          <v-icon color="#D22B2B">account_balance</v-icon>
+                          <v-card-text class="text-h4 font-weight-bold">
+                            50% Employess from Harvard/Stanford University
+                          </v-card-text>
                         <!-- </span> -->
                          <!-- margin-top: 10px -->
+                         <!-- style="font-size: 44px;" -->
                       </v-col>
 
                     </v-row>
+                  </v-card-text>
+
+                  <v-card-title class="justify-center">
+                    <h1 class="text-h3 font-weight-thin mb-2">
+                      Our Accomplishments!
+                    </h1>
+                  </v-card-title>
+
+                  <v-card-text>
+                    <v-row>
+                      <!-- align-content="center" -->
+                      <!-- align="center" -->
+                      <v-col style="text-align:center; display:inline-block;" align-content="center" cols="4" class="fill-height">
+                        <!-- <span> -->
+                          <v-icon color="#D22B2B">feed</v-icon>
+                          <v-card-text class="text-h4 font-weight-bold">
+                            Over 300+ Clients
+                          </v-card-text>
+                        <!-- </span> -->
+                        <!-- large -->
+                      </v-col>
+
+                      <v-col style="text-align:center; display:inline-block;" align-content="center" cols="4" class="fill-height">
+                        <!-- <span> -->
+                          <v-icon color="#D22B2B">account_group_outline</v-icon>
+                          <v-card-text class="text-h4 font-weight-bold">
+                            Voted Most Promising Statup 2022 by the Times group!
+                          </v-card-text>
+                        <!-- </span> -->
+                        <!-- style="font-size: 44px;" -->
+                      </v-col>
+
+                      <v-col style="text-align:center; display:inline-block;" align-content="center" cols="4" class="fill-height">
+                        <!-- <span> -->
+                          <v-icon color="#D22B2B">account_balance</v-icon>
+                          <v-card-text class="text-h4 font-weight-bold">
+                            100% Customer Satisfaction
+                          </v-card-text>
+                        <!-- </span> -->
+                         <!-- margin-top: 10px -->
+                         <!-- style="font-size: 44px;" -->
+                      </v-col>
+
+                    </v-row>
+                  </v-card-text>
+
+                  <v-card-title class="justify-left">
+                    <h1 class="text-h3 font-weight-thin mb-2">
+                      Leadership
+                    </h1>
+                  </v-card-title>
+
+                  <v-card-text class="text-h6 text-justify">
+                    Our leaders reflect our values of teamwork, transparency, and innovation.
+                    And they’re committed to making the impossible possible. <br> <br>
+
+                    <a href="">Meet the leadership team ---></a>
                   </v-card-text>
                 
                 </v-parallax>
               </v-tab-item>
 
+              <v-tab-item>
+                <v-parallax
+                  height="100%"
+                  src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+                >
+                  <v-card-title class="justify-center">
+                    <h1 class="text-h2 font-weight-thin mb-2">
+                      Company Policy
+                    </h1>
+                  </v-card-title>
+
+                  <v-list-item v-for="compPol in companyPolicy" :key="compPol.Id" class="text-justify">
+                    <v-list-item-content>
+                        <v-card-text>
+                          <v-list-item-sub-title class="text-h6 font-weight-thin mb-2">{{compPol.content}}</v-list-item-sub-title>
+                        </v-card-text>
+                      </v-list-item-content>
+                  </v-list-item>
+                  
+                </v-parallax>
+              </v-tab-item>
+
+              <v-tab-item>
+                <v-parallax
+                  height="100%"
+                  src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+                >
+                <!-- :src="require('@/assets/mountain.png')" -->
+                <!-- https://cdn.vuetifyjs.com/images/parallax/material2.jpg -->
+                  <v-card-title class="justify-center">
+                    <h1 class="text-h2 font-weight-thin mb-2">
+                      Employee Policy
+                    </h1>
+                  </v-card-title>
+
+                  <v-list-item v-for="empPol in employeePolicy" :key="empPol.Id" class="text-justify">
+                    <v-list-item-content>
+                        <v-card-text>
+                          <v-list-item-sub-title class="text-h6 font-weight-thin mb-2">{{empPol.content}}</v-list-item-sub-title>
+                        </v-card-text>
+                      </v-list-item-content>
+                  </v-list-item>
+                  
+                </v-parallax>
+              </v-tab-item>
             </v-tabs-items>
-            
-
           </v-card>
-
-
         </v-row>
       </v-layout>
     </v-container>
@@ -122,9 +234,65 @@ export default{
   },
   data: () => ({
     isClicked: false,
-    tabs:null,
-    tabName : ["About Me", "Company Policy", "Employee Policy"]
+    tabs:0,
+    tabName : ["About Us", "Company Policy", "Employee Policy", "Core Values", "Mission Statement"],
+    companyPolicy : [
+      {
+        Id : 1,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+      {
+        Id : 2,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+      {
+        Id : 3,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+      {
+        Id : 4,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+      {
+        Id : 5,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+      {
+        Id : 6,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+    ],
+    employeePolicy : [
+      {
+        Id : 1,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+      {
+        Id : 2,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+      {
+        Id : 3,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+      {
+        Id : 4,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+      {
+        Id : 5,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+      {
+        Id : 6,
+        content : "Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque. Inference is a powerful tool. You do it without thinking — your brain fills in the missing vowels in our company’s name effortlessly. It knows what’s missing. So do we. We have designed our technology to make these kinds of connections, and at lightning speed."
+      },
+    ],
   }),
 }
 
 </script>
+
+
+<style lang="scss" scoped>
+</style>
