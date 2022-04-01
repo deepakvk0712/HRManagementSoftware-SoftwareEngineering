6e0cbaf403bf823f2c1b42e2c02dff9fcecc3710
Deepak <deepak@Deepaks-MacBook-Pro.local>
2022-04-01 19:06:01 -0400

    Small change made in attendance page.

--- front-end/src/components/AttendancePage.vue
+++ front-end/src/components/AttendancePage.vue
@@ -334,16 +334,4 @@ export default {
 
 
 
-Added the following features to sprint 3:
 
-* Designed and Created the About us page.
-* Added company mission, company policy, employee policy, and core values to the about page.
-* Added about us tab containing information about the company and some key highlights.
-* Added fields manager name, business unit, and team members on the user profile page. Now, these fields will be displayed on the user profile page along with all the information we were already showing.
-* Made change to the Add employee form, now ill take employee salary as an addition (Done so that it can be used to generate the employee's payslip).
-* Designed and created the attendance page.
-* Integrated the API to insert the date and time worked by an employee with the backend.
-* Have designed the card to take input to start and end date to show the filtered stats. Currently, this work has been pushed to the next sprint and will be integrated then (There is a placeholder currently).
-* Fixed the issue with the change password page and the routing issues that were there.
-* Added 10+ test cases for the about us page.
-* Added test cases for the user profile page (For the additions made in this sprint).
