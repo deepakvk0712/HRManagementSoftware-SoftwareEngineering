0620f626cb0e16912c74d7ad521383637495d8f5
deepakvk0712 <90729944+deepakvk0712@users.noreply.github.com>
2022-04-20 22:24:31 -0400

    Update updateController_test.go

--- Backend/Controller/updateController_test.go
+++ Backend/Controller/updateController_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 	middleware "hrtool.com/HRManagementSoftware-SoftwareEngineering/Backend/Middleware"
-	models "hrtool.com/HRManagementSoftware-SoftwareEngineering/Backend/Models"
 	gormModels "hrtool.com/HRManagementSoftware-SoftwareEngineering/Backend/Models/GormModels"
 	utils "hrtool.com/HRManagementSoftware-SoftwareEngineering/Backend/Utils"
 	"net/http"
@@ -24,38 +23,12 @@ func TestUpdateEmployeeInfo(t *testing.T) {
 
 	updateRouter := router.PathPrefix("/users").Subrouter()
 	updateRouter.HandleFunc("/UpdateEmployeeInfo1", UpdateEmployeeInfo).Methods("POST")
+	updateRouter.HandleFunc("/UpdateEmployeeInfo2", UpdateEmployeeBankingInfo).Methods("POST")
 	updateRouter.Use(middleware.ValidateAccessToken)
 
 	utils.Init()
 
 	t.Run("Update employee info", func(t *testing.T) {
-		user := models.UserLogin{
-			Email:    "dstejas191@hrtools.com",
-			Password: "123",
-		}
-
-		payload, err := json.Marshal(&user)
-		assert.NoError(t, err)
-
-		loginRequest, err := http.NewRequest("POST", "/login", bytes.NewBuffer(payload))
-		loginRequest.Header.Set("Content-Type", "application/json")
-		assert.NoError(t, err)
-
-		w := httptest.NewRecorder()
-
-		router.ServeHTTP(w, loginRequest)
-		assert.Equal(t, 200, w.Code)
-
-		type targetType struct {
-			AccessToken string `json:"accessToken"`
-			FirstLogin  bool   `json:"firstLogin"`
-		}
-
-		response := models.JsonResponse{}
-		var target targetType
-
-		json.NewDecoder(w.Body).Decode(&response)
-		json.Unmarshal([]byte(response.Data), &target)
 
 		employeeInfo := gormModels.User{
 			EmployeeID:      34,
@@ -75,65 +48,21 @@ func TestUpdateEmployeeInfo(t *testing.T) {
 			Nationality:     "China",
 			Pronouns:        "Alex"}
 
-		payload, err = json.Marshal(&employeeInfo)
+		payload, err := json.Marshal(&employeeInfo)
 		assert.NoError(t, err)
 
 		updateRequest, err := http.NewRequest("POST", "/users/UpdateEmployeeInfo1", bytes.NewBuffer(payload))
 		updateRequest.Header.Set("Content-Type", "application/json")
-		updateRequest.Header.Set("Authorization", "Bearer "+target.AccessToken)
-		assert.NoError(t, err)
-
-		w = httptest.NewRecorder()
-
-		updateRouter.ServeHTTP(w, updateRequest)
-		assert.Equal(t, 200, w.Code)
-	})
-
-}
-
-func TestUpdateEmployeeBankingInfo(t *testing.T) {
-	router := mux.NewRouter()
-
-	loginRouter := router.PathPrefix("/login").Subrouter()
-	loginRouter.HandleFunc("", Login).Methods("POST")
-	loginRouter.Use(middleware.ValidateUser)
-	loginRouter.Use(middleware.Authorize)
-
-	updateRouter := router.PathPrefix("/users").Subrouter()
-	updateRouter.HandleFunc("/UpdateEmployeeInfo2", UpdateEmployeeBankingInfo).Methods("POST")
-	updateRouter.Use(middleware.ValidateAccessToken)
-
-	utils.Init()
-
-	t.Run("Update employee Banking info", func(t *testing.T) {
-
-		user := models.UserLogin{
-			Email:    "dstejas191@hrtools.com",
-			Password: "123",
-		}
-
-		payload, err := json.Marshal(&user)
-		assert.NoError(t, err)
-
-		loginRequest, err := http.NewRequest("POST", "/login", bytes.NewBuffer(payload))
-		loginRequest.Header.Set("Content-Type", "application/json")
+		token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImRzdGVqYXMxOTFAaHJ0b29scy5jb20iLCJTdGFuZGFyZENsYWltIjp7ImV4cCI6MTY1NDcwMTg2MywiaXNzIjoiTG9naW4ifSwiUm9sZSI6N30.jOH-ukfC09X0hjMSJsywpspU2GiP2_whAmLRlfRVVpM"
+		updateRequest.Header.Set("Authorization", "Bearer "+token)
 		assert.NoError(t, err)
 
 		w := httptest.NewRecorder()
 
-		router.ServeHTTP(w, loginRequest)
-		assert.Equal(t, 200, w.Code)
-
-		type targetType struct {
-			AccessToken string `json:"accessToken"`
-			FirstLogin  bool   `json:"firstLogin"`
-		}
-
-		response := models.JsonResponse{}
-		var target targetType
-
-		json.NewDecoder(w.Body).Decode(&response)
-		json.Unmarshal([]byte(response.Data), &target)
+		updateRouter.ServeHTTP(w, updateRequest)
+		assert.Equal(t, 201, w.Code)
+	})
+	t.Run("Update employee Banking info", func(t *testing.T) {
 
 		employeeInfo := gormModels.User{
 			EmployeeID:    34,
@@ -141,18 +70,18 @@ func TestUpdateEmployeeBankingInfo(t *testing.T) {
 			AccountNumber: "456",
 			Bank:          "Wells Fargo"}
 
-		payload, err = json.Marshal(&employeeInfo)
+		payload, err := json.Marshal(&employeeInfo)
 		assert.NoError(t, err)
 
 		updateRequest, err := http.NewRequest("POST", "/users/UpdateEmployeeInfo2", bytes.NewBuffer(payload))
 		updateRequest.Header.Set("Content-Type", "application/json")
-		updateRequest.Header.Set("Authorization", "Bearer "+target.AccessToken)
+		token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImRzdGVqYXMxOTFAaHJ0b29scy5jb20iLCJTdGFuZGFyZENsYWltIjp7ImV4cCI6MTY1NDcwMTg2MywiaXNzIjoiTG9naW4ifSwiUm9sZSI6N30.jOH-ukfC09X0hjMSJsywpspU2GiP2_whAmLRlfRVVpM"
+		updateRequest.Header.Set("Authorization", "Bearer "+token)
 		assert.NoError(t, err)
 
-		w = httptest.NewRecorder()
+		w := httptest.NewRecorder()
 
 		updateRouter.ServeHTTP(w, updateRequest)
-		assert.Equal(t, 200, w.Code)
+		assert.Equal(t, 201, w.Code)
 	})
-
 }
