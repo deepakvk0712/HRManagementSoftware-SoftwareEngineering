063a9571dd54b1a8d53eec0b14c35e1b4e3bbcfe
deepakvk0712 <90729944+deepakvk0712@users.noreply.github.com>
2022-04-20 22:23:59 -0400

    Update workingController_test.go

--- Backend/Controller/workingController_test.go
+++ Backend/Controller/workingController_test.go
@@ -13,110 +13,75 @@ import (
 	"testing"
 )
 
-func TestGetSchedule(t *testing.T) {
+func TestSchedule(t *testing.T) {
 	router := mux.NewRouter()
-
-	loginRouter := router.PathPrefix("/login").Subrouter()
-	loginRouter.HandleFunc("", Login).Methods("POST")
-	loginRouter.Use(middleware.ValidateUser)
-	loginRouter.Use(middleware.Authorize)
 
 	workingRouter := router.PathPrefix("/working").Subrouter()
 	workingRouter.HandleFunc("/getSchedule", GetSchedule).Methods("GET")
 	workingRouter.HandleFunc("/getWorkingDetails", GetWorkingDetailsBetween).Methods("GET")
+	workingRouter.HandleFunc("/insertWorkingRecord", SetWorkingHours).Methods("POST")
+
 	workingRouter.Use(middleware.ValidateAccessToken)
 
 	utils.Init()
 
 	t.Run("Get schedule", func(t *testing.T) {
-		user := models.UserLogin{
-			Email:    "dstejas191@hrtools.com",
-			Password: "123",
-		}
-
-		payload, err := json.Marshal(&user)
-		assert.NoError(t, err)
-
-		loginRequest, err := http.NewRequest("POST", "/login", bytes.NewBuffer(payload))
-		loginRequest.Header.Set("Content-Type", "application/json")
-		assert.NoError(t, err)
-
-		w := httptest.NewRecorder()
-
-		router.ServeHTTP(w, loginRequest)
-		assert.Equal(t, 200, w.Code)
-
-		type targetType struct {
-			AccessToken string `json:"accessToken"`
-			FirstLogin  bool   `json:"firstLogin"`
-		}
-
-		response := models.JsonResponse{}
-		var target targetType
-
-		json.NewDecoder(w.Body).Decode(&response)
-		json.Unmarshal([]byte(response.Data), &target)
 
 		EmployeeID := 34
-		payload, err = json.Marshal(&EmployeeID)
+		payload, err := json.Marshal(&EmployeeID)
 		assert.NoError(t, err)
 
 		scheduleRequest, err := http.NewRequest("GET", "/working/getSchedule", bytes.NewBuffer(payload))
 		scheduleRequest.Header.Set("Content-Type", "application/json")
-		scheduleRequest.Header.Set("Authorization", "Bearer "+target.AccessToken)
+		token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImRzdGVqYXMxOTFAaHJ0b29scy5jb20iLCJTdGFuZGFyZENsYWltIjp7ImV4cCI6MTY1NDcwMTg2MywiaXNzIjoiTG9naW4ifSwiUm9sZSI6N30.jOH-ukfC09X0hjMSJsywpspU2GiP2_whAmLRlfRVVpM"
+		scheduleRequest.Header.Set("Authorization", "Bearer "+token)
 		assert.NoError(t, err)
 
-		w = httptest.NewRecorder()
+		w := httptest.NewRecorder()
 
 		workingRouter.ServeHTTP(w, scheduleRequest)
 		assert.Equal(t, 200, w.Code)
 	})
 
 	t.Run("Get Working Details", func(t *testing.T) {
-		user := models.UserLogin{
-			Email:    "dstejas191@hrtools.com",
-			Password: "123",
-		}
-
-		payload, err := json.Marshal(&user)
-		assert.NoError(t, err)
-
-		loginRequest, err := http.NewRequest("POST", "/login", bytes.NewBuffer(payload))
-		loginRequest.Header.Set("Content-Type", "application/json")
-		assert.NoError(t, err)
-
-		w := httptest.NewRecorder()
-
-		router.ServeHTTP(w, loginRequest)
-		assert.Equal(t, 200, w.Code)
-
-		type targetType struct {
-			AccessToken string `json:"accessToken"`
-			FirstLogin  bool   `json:"firstLogin"`
-		}
-
-		response := models.JsonResponse{}
-		var target targetType
-
-		json.NewDecoder(w.Body).Decode(&response)
-		json.Unmarshal([]byte(response.Data), &target)
 
 		FD := models.FrontendDate{}
-		FD.StartDate = "2022-03-31"
-		FD.EndDate = "2022-04-05"
+		FD.StartDate = "2022-04-01"
+		FD.EndDate = "2022-04-15"
 
-		payload, err = json.Marshal(&FD)
+		payload, err := json.Marshal(&FD)
 		assert.NoError(t, err)
 
 		weekHRequest, err := http.NewRequest("GET", "/working/getWorkingDetails", bytes.NewBuffer(payload))
 		weekHRequest.Header.Set("Content-Type", "application/json")
-		weekHRequest.Header.Set("Authorization", "Bearer "+target.AccessToken)
+		token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImRzdGVqYXMxOTFAaHJ0b29scy5jb20iLCJTdGFuZGFyZENsYWltIjp7ImV4cCI6MTY1NDcwMTg2MywiaXNzIjoiTG9naW4ifSwiUm9sZSI6N30.jOH-ukfC09X0hjMSJsywpspU2GiP2_whAmLRlfRVVpM"
+		weekHRequest.Header.Set("Authorization", "Bearer "+token)
 		assert.NoError(t, err)
 
-		w = httptest.NewRecorder()
+		w := httptest.NewRecorder()
 
 		workingRouter.ServeHTTP(w, weekHRequest)
 		assert.Equal(t, 200, w.Code)
 	})
+	t.Run("Insert Working Record", func(t *testing.T) {
 
+		FT := models.FrontendTime{}
+		FT.Date = "2022-04-17"
+		FT.StartTime = "09:00"
+		FT.EndTime = "17:00"
+
+		payload, err := json.Marshal(&FT)
+		assert.NoError(t, err)
+
+		weekHRequest, err := http.NewRequest("POST", "/working/insertWorkingRecord", bytes.NewBuffer(payload))
+		weekHRequest.Header.Set("Content-Type", "application/json")
+		token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImRzdGVqYXMxOTFAaHJ0b29scy5jb20iLCJTdGFuZGFyZENsYWltIjp7ImV4cCI6MTY1NDcwMTg2MywiaXNzIjoiTG9naW4ifSwiUm9sZSI6N30.jOH-ukfC09X0hjMSJsywpspU2GiP2_whAmLRlfRVVpM"
+		weekHRequest.Header.Set("Authorization", "Bearer "+token)
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+
+		workingRouter.ServeHTTP(w, weekHRequest)
+		assert.Equal(t, 201, w.Code)
+	})
 }
